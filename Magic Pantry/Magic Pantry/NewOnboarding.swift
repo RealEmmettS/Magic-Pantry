2a9588c77468e98ff85d0432ce37fa8bf864e8d0
Emmett S <30877743+RealEmmettS@users.noreply.github.com>
2021-02-22 20:14:36 -0600

    fixed fonts and onboarding

--- Magic Pantry/Magic Pantry/NewOnboarding.swift
+++ Magic Pantry/Magic Pantry/NewOnboarding.swift
@@ -1,19 +1,38 @@
 //
-//  onboarding.swift
+//  NewOnboarding.swift
 //  Magic Pantry
 //
-//  Created by Emmett Shaughnessy on 2/21/21.
+//  Created by Emmett Shaughnessy on 2/22/21.
 //  Copyright © 2021 Emmett Shaughnessy. All rights reserved.
 //
 
 import UIKit
 import paper_onboarding
 
-class onboarding: PaperOnboarding {
+class NewOnboarding: UIViewController {
 
+    @IBOutlet weak var onboardingView: PaperOnboarding!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        onboardingView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
+
+
+extension NewOnboarding: PaperOnboardingDataSource{
+    func onboardingItemsCount() -> Int {
+        return 2
+    }
+    
     func onboardingItem(at index: Int) -> OnboardingItemInfo {
-        
-        guard let MPTitleFont = UIFont(name: "PublicSans-ExtraBold.otf", size: UIFont.labelFontSize) else {
+        guard let MPTitleFont = UIFont(name: "PublicSans-ExtraBold", size: UIFont.labelFontSize) else {
             fatalError("""
                 Failed to load the "PublicSans-ExtraBold.otf" font.
                 Make sure the font file is included in the project and the font name is spelled correctly.
@@ -21,7 +40,7 @@ class onboarding: PaperOnboarding {
             )
         }
         
-        guard let MPDescFont = UIFont(name: "PublicSans-Thin.otf", size: UIFont.labelFontSize) else {
+        guard let MPDescFont = UIFont(name: "PublicSans-Thin", size: UIFont.labelFontSize) else {
             fatalError("""
                 Failed to load the "PublicSans-ExtraBold.otf" font.
                 Make sure the font file is included in the project and the font name is spelled correctly.
@@ -29,43 +48,38 @@ class onboarding: PaperOnboarding {
             )
         }
         
-        let MPRedColor = UIColor(red: 250, green: 62, blue: 62, alpha: 1)
+        let MPRedColor = UIColor(red: 0.92, green: 0.23, blue: 0.23, alpha: 1.00)
 
        return [
-        OnboardingItemInfo(informationImage: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+        OnboardingItemInfo(informationImage: UIImage(named: "MPLogo")!,
                                        title: "title",
                                  description: "description",
-                                 pageIcon: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+                                 pageIcon: UIImage(named: "MPLogo")!,
                                        color: MPRedColor,
                                   titleColor: UIColor.white,
                             descriptionColor: UIColor.white,
                             titleFont: MPTitleFont,
                              descriptionFont: MPDescFont),
 
-        OnboardingItemInfo(informationImage: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+        OnboardingItemInfo(informationImage: UIImage(named: "MPLogo")!,
                                         title: "title",
                                   description: "description",
-                                  pageIcon: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+                                  pageIcon: UIImage(named: "MPLogo")!,
                                         color: MPRedColor,
                                    titleColor: UIColor.white,
                              descriptionColor: UIColor.white,
                              titleFont: MPTitleFont,
                               descriptionFont: MPDescFont),
 
-        OnboardingItemInfo(informationImage: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+        OnboardingItemInfo(informationImage: UIImage(named: "MPLogo")!,
                                      title: "title",
                                description: "description",
-                               pageIcon: UIImage(imageLiteralResourceName: "ItunesArtwork@2x.png"),
+                               pageIcon: UIImage(named: "MPLogo")!,
                                color: MPRedColor,
                                 titleColor: UIColor.white,
                           descriptionColor: UIColor.white,
                             titleFont: MPTitleFont,
                            descriptionFont: MPDescFont)
          ][index]
-     }
-
-     func onboardingItemsCount() -> Int {
-        return 3
-      }
-
+    }
 }
